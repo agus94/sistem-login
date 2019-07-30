4c7adfc527fa18f0cd1de54cc3990572aeb2df6e
agus94 <agusismail94@gmail.com>
2019-07-30 21:41:06 +0700

    menambah form validasi pada menit 10:50

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
+	public function __construct() 
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
 		$this->load->view('templates/auth_header');
@@ -12,7 +18,10 @@ class Auth extends CI_Controller {
 
 	public function registration()
 	{
-		$this->load->view('templates/auth_header');
+		$data = array(
+			'title' => 'WPU Sistem Login',
+		);
+		$this->load->view('templates/auth_header', $data);
 		$this->load->view('auth/registration');
 		$this->load->view('templates/auth_footer');
 	}
