2574e83b3b02aed6826052336c1680c04f778e5c
agus94 <agusismail94@gmail.com>
2019-08-01 16:47:01 +0700

    menambahkan validasi di kantor tapi gagal

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,16 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
-	public function __construct() 
-	{
-		parent::__construct();
-		$this->load->library('form_validation');
-	}
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
 
 	public function index()
 	{
 		$data['title'] = 'WPU User Login';
-		$this->load->view('templates/auth_header');
+		$this->load->view('templates/auth_header', $data);
 		$this->load->view('auth/login');
 		$this->load->view('templates/auth_footer');
 	}
@@ -20,16 +20,19 @@ class Auth extends CI_Controller {
 	public function registration()
 	{
 		$this->form_validation->set_rules('name', 'Name', 'required|trim');
-		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
+		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]');
+		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 		
-		if( $this->form_validation->run() == false ) {
-			$data['title'] = 'WPU User Registration';
-			$this->load->view('templates/auth_header', $data);
-			$this->load->view('auth/registration');
-			$this->load->view('templates/auth_footer');
-		} else {
-			echo 'data berhasil ditambahkan';
-		}
+		if ($this->form_validation->run() == FALSE)
+		{
+					$data['title'] = 'WPU User Registration';
+					$this->load->view('templates/auth_header', $data);
+					$this->load->view('auth/registration');
+					$this->load->view('templates/auth_footer');
+        } else {
+					echo 'data berhasil ditambahkan';
+        }
 	}
 
 }
