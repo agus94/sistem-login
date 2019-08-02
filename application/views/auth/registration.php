b6094ae07074b9ddfac626f9fab91b9e9838de2d
agus94 <agusismail94@gmail.com>
2019-08-02 14:18:11 +0700

    menambahkan fitur User Registration

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -9,13 +9,13 @@
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
               </div>
-              <form class="user" action="POST" action="<?= base_url('auth/registration'); ?>">
+              <form class="user" method="POST" action="<?= base_url('auth/registration'); ?>">
                 <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full name">
+                  <input type="text" class="form-control form-control-user" id="name" name="name" value="<?= set_value('name'); ?>" placeholder="Full name">
                   <?= form_error('name', '<small class="text-danger pl-3">','</small>'); ?>
                 </div>
                 <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
+                  <input type="text" class="form-control form-control-user" id="email" name="email" value="<?= set_value('email'); ?>" placeholder="Email Address">
                   <?= form_error('email', '<small class="text-danger pl-3">','</small>'); ?>
                 </div>
                 <div class="form-group row">
